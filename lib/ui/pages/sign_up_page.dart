a1c4ed20efac9fd21c367e499f310b503cd4e38f
hitnesmuh <hitmuharr@gmail.com>
2022-07-17 21:40:58 +0700

    Create Airplane Application with Flutter

---
+++ lib/ui/pages/sign_up_page.dart
@@ -0,0 +1,160 @@
+import 'package:airplane/cubit/auth_cubit.dart';
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/widgets/custom_button.dart';
+import 'package:airplane/ui/pages/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpPage extends StatelessWidget {
+  SignUpPage({Key? key}) : super(key: key);
+
+  TextEditingController nameController = TextEditingController(text: '');
+  TextEditingController emailController = TextEditingController(text: '');
+  TextEditingController passwordController = TextEditingController(text: '');
+  TextEditingController hobbyController = TextEditingController(text: '');
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Text(
+          'Join us and get\nyour next journey',
+          style: blackTextStyle.copyWith(
+            fontSize: 24,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget inputSection() {
+      Widget nameInput() {
+        return CustomTextFormField(
+          title: 'Full Name',
+          hintText: 'Your Full Name',
+          controller: nameController,
+        );
+      }
+
+      Widget emailInput() {
+        return CustomTextFormField(
+          title: 'Email Address',
+          hintText: 'Your Email Adrres',
+          controller: emailController,
+        );
+      }
+
+      Widget passwordInput() {
+        return CustomTextFormField(
+          title: 'Password',
+          hintText: 'Your Password',
+          obscureText: true,
+          controller: passwordController,
+        );
+      }
+
+      Widget hobbyInput() {
+        return CustomTextFormField(
+          title: 'Hobby',
+          hintText: 'Your Hobby',
+          controller: hobbyController,
+        );
+      }
+
+      Widget submitButton() {
+        return BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthSuccess) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/bonus', (route) => false);
+            } else if (state is AuthFailed) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: kRedColor,
+                  content: Text(state.error),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return CustomButton(
+              title: 'Get Started',
+              onPressed: () {
+                context.read<AuthCubit>().signUp(
+                      email: emailController.text,
+                      password: passwordController.text,
+                      name: nameController.text,
+                      hobby: hobbyController.text,
+                    );
+              },
+            );
+          },
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(
+            defaultRadius,
+          ),
+        ),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            hobbyInput(),
+            submitButton(),
+          ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(
+          top: 20,
+          bottom: 73,
+        ),
+        child: Text(
+          'Terms and Condition',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          children: [
+            title(),
+            inputSection(),
+            tacButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
